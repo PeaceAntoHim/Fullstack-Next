91f9d2cdcd3c50646b1ed81aec1dbaa56e4b41a4
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-01-26 23:18:22 +0700

    scema user

--- pages/api/posts/delete/[id].js
+++ pages/api/posts/delete/[id].js
@@ -4,7 +4,7 @@ import db from '../../../../libs/db';
 /* Ini untuk delete data use DELETE in postman */
 export default async function handler(req, res) {
     if(req.method !== 'DELETE') return res.status(405).end();
-
+    
     /* destructuring */
     const { id } = req.query;
 
