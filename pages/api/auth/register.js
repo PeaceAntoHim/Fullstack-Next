fa666c35361182a786062ab655308daf3a2c3461
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-01-27 22:11:49 +0700

    made JWT web token to user login

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,4 +1,5 @@
 import db from '../../../libs/db';
+import bcrypt from 'bcryptjs';
 
 export default async function handler(req, res) {
     if(req.method !== 'POST') return res.status(405).end();
@@ -6,10 +7,23 @@ export default async function handler(req, res) {
     /* distrukturing */
     const { email, password } = req.body;
 
-    console.log ({ email, password });
+    // console.log ({ email, password });
+    const salt = bcrypt.genSaltSync(10);
+    // console.log(salt);
+    const passwordHash = bcrypt.hashSync(password, salt);
+    // console.log(passwordHash);4
+    const register = await db('users').insert({
+        email,
+        password: passwordHash
+    });
+
+    const registeredUser = await db('users')
+                            .where({ id:register })
+                            .first();
 
     res.status(200);
     res.json({
-        message: 'Hello world'
+        message: 'User registerd successfully',
+        data: registeredUser
     });
 }
