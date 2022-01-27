945b8c5feb02676d56cb5c9de86d3f0470fb291b
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-01-27 22:51:22 +0700

    verivikasi JWT Token

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -1,8 +1,31 @@
 import db from '../../../libs/db';
+import jwt from 'jsonwebtoken';
 
 /* ini untuk Read data use get in Postman  */
+
+/* Contoh middleware */
 export default async function hendler(req, res) {
+    const { authorization } = req.headers;
+    // console.log(req.headers);
+    
     if(req.method !== 'GET') return res.status(405).end();
+
+    /* Distructuring Object */
+    const authSplit = authorization.split(' ');
+    // console.log(authSplit)
+    const [authType, authToken] = [
+        authSplit[0],
+        authSplit[1]
+    ]
+
+    if(authType !== 'Bearer') return res.status(401).end();
+
+    const verify = jwt.verify(authToken, 'greteGod');
+
+    console.log(verify);
+ 
+    if(req.method !== 'GET') return res.status(405).end();
+
 
     const posts = await db('posts');
 
