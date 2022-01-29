240070be621156a8365e53d1215cfbdfc8c09994
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-01-29 22:29:06 +0700

    get access from auth/login api

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -1,12 +1,14 @@
 import db from '../../../libs/db';
-import authorization from '../../../libs/authorization';
+import authorization from '../../../middlewares/authorization';
 
 export default async function handler(req, res) {
     if(req.method !== 'GET') return res.status(405).end('');
+    console.log(req.headers);
 
     const auth = await authorization(req, res);
+    // console.log(auth);
 
-    const auth = await db('posts');
+    const posts  = await db('posts');
 
     res.status(200);
     res.json({
