3af2e3fa46103cd8a5ca5871bfdcb73242d5862f
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-01-30 11:48:20 +0700

    made react hook to handler form when we write

--- pages/auth/register.js
+++ pages/auth/register.js
@@ -1,19 +1,45 @@
+import React, { useState } from 'react';
+
 export default function Register() {
+    /* use React state */
+    const [feilds, setFeilds] = useState({
+        email: '',
+        password: ''
+    });
+
     
-    function registerHandler(e) {
+    async function registerHandler(e) {
         /* e.preventDefault itu adalah unutk mencegah fungsi bawaan dari broeser pergi ke action atau mencegah relod */
         e.preventDefault();
-        console.log('Submit form');
+        // console.log(feilds);
+    
+        /* Hit to register api */
+        const registerReq = await fetch('/api/auth/register', {
+            method: 'POST',
+            body: JSON.stringify(feilds)
+        });
+
+        const registerRes = await registerReq.json();
+        console.log(registerRes);
     }
     
+    function feildHandler(e) {
+        // setFeilds({ email: e.target.value });
+        const name = e.target.getAttribute('name');
+
+        setFeilds({
+            ...feilds,
+            [name]: e.target.value
+        });
+    }
     
     return (
         <div>
             <h1>Register</h1>
 
             <form onSubmit={registerHandler.bind(this)}>
-                <input type="text" placeholder="Email" /><br/>
-                <input type="password" placeholder="Password" />
+                <input name="email" onChange={feildHandler.bind(this)} type="text" placeholder="Email" /><br/>
+                <input name="password" onChange={feildHandler.bind(this)} type="password" placeholder="Password" />
                 <br />
                 <button type="submit">
                     Register
